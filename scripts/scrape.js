4e0306b8dfe44df2f7a0108998e99dfbbe692a3f
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-01-26 14:46:12 -0700

    WebKit to Webkit, and updates property list, fixes #5

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -10,10 +10,10 @@ superagent.get(url)
       .map((x) => x.children[0].children[0].data)
       .filter((x) => /^[a-z-]+$/.test(x))
     var camel = names.map((x) => x.split('-').map((a, i) => i === 0 ? a : a[0].toUpperCase() + a.slice(1)).join(''));
-    camel.push('fontSize');
+    camel.push('fontSize', 'flex', 'fr');
     var prefixed = camel.map((name) => {
       var upper = name[0].toUpperCase() + name.slice(1);
-      return [name, 'Moz' + upper, 'WebKit' + upper, 'MS' + upper, 'O' + upper];
+      return [name, 'Moz' + upper, 'Webkit' + upper, 'MS' + upper, 'O' + upper];
     }).reduce((xs, x) => xs.concat(x), []);
     console.log('module.exports = ' + JSON.stringify(prefixed, null, 2));
   });
